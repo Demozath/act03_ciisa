cca160ed4da2b0ddc1e67c8991843cbe7afc24ca
Demozath <abeljsmz@gmail.com>
2023-06-25 23:49:40 -0400

    Se agrega geolocalizacion

--- app/src/main/java/com/demozath/act03_ciisa/LobbyActivity.java
+++ app/src/main/java/com/demozath/act03_ciisa/LobbyActivity.java
@@ -1,15 +1,65 @@
 package com.demozath.act03_ciisa;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class LobbyActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class LobbyActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
+
+    EditText etLatitud, etLongitud;
+    GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+        etLatitud = findViewById(R.id.etLatitud);
+        etLongitud = findViewById(R.id.etLongitud);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
-}
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        mMap = googleMap;
+        this.mMap.setOnMapClickListener(this);
+        this.mMap.setOnMapLongClickListener(this);
+
+        LatLng chile = new LatLng(-33.44895975817196, -70.66815268353263);
+        mMap.addMarker(new MarkerOptions().position(chile).title("Chile"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(chile));
+
+    }
+
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+
+        double latitude = latLng.latitude;
+        double longitude = latLng.longitude;
+        etLatitud.setText(String.valueOf(latitude));
+        etLongitud.setText(String.valueOf(longitude));
+
+    }
+
+    @Override
+    public void onMapLongClick(@NonNull LatLng latLng) {
+        double latitude = latLng.latitude;
+        double longitude = latLng.longitude;
+        etLatitud.setText(String.valueOf(latitude));
+        etLongitud.setText(String.valueOf(longitude));
+    }
+
+
+    }
