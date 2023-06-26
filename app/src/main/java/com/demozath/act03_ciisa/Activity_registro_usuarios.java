d24421687064cab8de27e879f7492fac93e91546
Demozath <abeljsmz@gmail.com>
2023-06-25 23:19:31 -0400

    Se generan documentos faltantse

---
+++ app/src/main/java/com/demozath/act03_ciisa/Activity_registro_usuarios.java
@@ -0,0 +1,14 @@
+package com.demozath.act03_ciisa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_registro_usuarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_usuarios);
+    }
+}
